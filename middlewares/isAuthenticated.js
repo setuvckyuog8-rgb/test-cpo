c9bb3b8c616a107dc64fdab923f5a92dc584c71f
setuvckyuog8-rgb <setuvckyuog8@gmail.com>
2025-12-24 09:49:13 +0530

    Something fissy

--- middlewares/isAuthenticated.js
+++
@@ -1,7 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated && req.isAuthenticated()) {
-    return next();
-  }
-  return res.status(401).json({ message: "Unauthorized: Please login first" });
-}
-module.exports = isAuthenticated;
